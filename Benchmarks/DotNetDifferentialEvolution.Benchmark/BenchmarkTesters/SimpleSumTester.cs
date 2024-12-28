dd58a86756eee091d63953e9e4f6d877b36f74d0
Eduard Burachek <baryon-asymm@MacBook-Air-Eduard.local>
2024-12-29 00:13:56 +0300

    fix for the benchmark test

--- Benchmarks/DotNetDifferentialEvolution.Benchmark/BenchmarkTesters/SimpleSumTester.cs
+++ Benchmarks/DotNetDifferentialEvolution.Benchmark/BenchmarkTesters/SimpleSumTester.cs
@@ -8,6 +8,8 @@ using DotNetDifferentialEvolution.MutationStrategies.Interfaces;
 using DotNetDifferentialEvolution.RandomProviders;
 using DotNetDifferentialEvolution.SelectionStrategies;
 using DotNetDifferentialEvolution.SelectionStrategies.Interfaces;
+using DotNetDifferentialEvolution.TerminationStrategies;
+using DotNetDifferentialEvolution.Tests.Shared.FitnessFunctionEvaluators;
 using DotNetDifferentialEvolution.Tests.Shared.Helpers;
 
 namespace DotNetDifferentialEvolution.Benchmark.BenchmarkTesters;
@@ -23,7 +25,20 @@ public class SimpleSumTester
     
     public SimpleSumTester()
     {
-        var context = ProblemContextHelper.CreateContext();
+        const int genomeSize = 20;
+        var lowerBounds = new double[genomeSize];
+        var upperBounds = new double[genomeSize];
+        for (var i = 0; i < genomeSize; i++)
+        {
+            lowerBounds[i] = -10;
+            upperBounds[i] = 10;
+        }
+        
+        var evaluator = new SimpleSumEvaluator(lowerBounds, upperBounds);
+        const int maxGenerationNumber = 100; // Not used in this benchmark
+        var terminationStrategy = new LimitGenerationNumberTerminationStrategy(maxGenerationNumber); // ...also not used
+        const int populationSize = 300;
+        var context = ProblemContextHelper.CreateContext(populationSize, evaluator, terminationStrategy);
 
         const int seed = 0x12345678;
         _randomProvider = new DeterminedRandomProvider(seed);
