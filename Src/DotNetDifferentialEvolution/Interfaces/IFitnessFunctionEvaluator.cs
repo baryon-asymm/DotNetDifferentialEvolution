e2e14f0705b9ece48e6c2ea31ea7ecfad2a48164
Eduard Burachek <baryon-asymm@MacBook-Air-Eduard.local>
2025-01-05 04:47:12 +0300

    Add XML docs of the public interfaces

--- Src/DotNetDifferentialEvolution/Interfaces/IFitnessFunctionEvaluator.cs
+++ Src/DotNetDifferentialEvolution/Interfaces/IFitnessFunctionEvaluator.cs
@@ -1,7 +1,20 @@
 namespace DotNetDifferentialEvolution.Interfaces;
 
+/// <summary>
+/// Represents a mechanism for evaluating the fitness value of a set of genes 
+/// in an optimization problem. The goal is to minimize the fitness value, 
+/// where lower values indicate better solutions.
+/// </summary>
 public interface IFitnessFunctionEvaluator
 {
+    /// <summary>
+    /// Evaluates the fitness value of a candidate solution represented by a set of genes.
+    /// The fitness function determines the quality of the solution in the context of the optimization problem.
+    /// </summary>
+    /// <param name="genes">A read-only span of <see cref="double"/> values representing the genes 
+    /// (parameters) of the candidate solution to be evaluated.</param>
+    /// <returns>A <see cref="double"/> representing the computed fitness value of the given genes.
+    /// Lower values indicate better solutions.</returns>
     public double Evaluate(
         ReadOnlySpan<double> genes);
 }
